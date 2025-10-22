c2f19e0556d51338276de4bb999dd086a79ed801
Christian Brandenburg <31166454+ChristianBrandenburg@users.noreply.github.com>
2025-10-22 21:23:26 +0200

    Typo fix
    Updated line 28 typo

--- usb-startup.sh
+++ usb-startup.sh
@@ -25,6 +25,6 @@ cp "${LXC_CONFIG_PATH}" "${LXC_CONFIG_PATH}.bak"
 
 # Replace the existing lxc.mount.entry line
 sed -i "/lxc.mount.entry: \/dev\/bus\/usb\//d" "${LXC_CONFIG_PATH}"
-echo "lxc.mount.entry: /dev/bus/usb/${USB_BUS_DEV// /\/} dev/bus/usb/${USB_BUS_DEV// /\/} none bind,optional,create=file" >> "${LXC_CON>
+echo "lxc.mount.entry: /dev/bus/usb/${USB_BUS_DEV// /\/} dev/bus/usb/${USB_BUS_DEV// /\/} none bind,optional,create=file" >> "${LXC_CONFIG_PATH}"
 
 logger -p user.info -t usb-lxc-setup "The USB device ${USB_DEV_NAME} has been successfully added to the LXC configuration."
